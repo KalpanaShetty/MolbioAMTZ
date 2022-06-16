43fc117801c30fe9d762ecce0e4b4b6389d6f326
KalpanaShetty <kalpanashetty512@gmail.com>
2022-06-16 11:23:07 +0530

    jenkins ready

---
+++ src/test/java/production/AmtzProductionArea.java
@@ -0,0 +1,10 @@
+package production;
+
+public class AmtzProductionArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sample for jenkins");
+	}
+
+}
